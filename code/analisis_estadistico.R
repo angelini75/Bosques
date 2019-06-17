cbbae0424710f1f2008bef3c0053dd7cc67604bf
Angelini <angelini75@gmail.com>
2019-06-17 18:31:28 -0300

    estimación de \hat{t}_h y \hat{p}_h (superficie y proporciion bien clasificada)

--- code/analisis_estadistico.R
+++ code/analisis_estadistico.R
@@ -1,4 +1,43 @@
 rm(list = ls())
 name <- function(x) { as.data.frame(names(x))}
 load(file = "results/segmentos.RData")
+rp <- rp[-1,]
+library(mapview)
+library(tidyverse) # requiere entender https://r4ds.had.co.nz/transform.html
 
+# Por ahora nos quedamos con los segmentos con datos completos
+Y <- Y[complete.cases(Y),]
+
+# # primero creamos una tabla con los estratos (e) y sus superficies
+# name(Y)
+# (e <- unique(Y[, c("estrato", "provincia", "region", "area.estrato")]))
+# rownames(e) <- 1:length(e[,1])
+
+# A_h es el área del estrato
+names(e)[4] <- "A_h"
+
+# y_hi = área de los segmentos bien clasificados
+Y$y_hi <- 0 # 0 para aquellos segmentos mal clasificados
+Y$y_hi[Y$unsef == Y$unlu] <- Y$ha[Y$unsef == Y$unlu]
+
+# veamos el porcentage de superficie total bien clasificada (sólo informativo)
+sum(Y$y_hi)/sum(Y$ha)
+
+# ahora empezamos a usar tidyverse
+(y <- as.tbl(Y))
+y$estrato <- y$estrato %>% as.factor
+
+# Agrupamos por estratos
+e <- y %>% group_by(estrato, region, provincia, area.estrato) %>% 
+  # pi_i es la probabilidad de inclusión del segmento i en el estrato h
+  # \hat{t)_h = sum{i=1}^{n_h} y_{hi}/pi_{hi} with 0
+  # pi_{hi} = n_h/N_h 
+  # n_h is sample size of stratum h, 
+  # N_h total number of segments in stratum h, and 
+  # y_{hi} the area correctly classified in segment i of stratum h.
+  summarise(n_h = sum(ha), # area total muestreada dentro del segmento 
+            N_h = unique(area.estrato), # cantidad de segmentos
+            pi_hi = n_h/N_h, # probabilidad de inclusión del segmento
+            hatt_h = sum(y_hi / pi_hi), # area bien clasificada
+            hatp_h = hatt_h/N_h) # proporción bien clasificada
+e
