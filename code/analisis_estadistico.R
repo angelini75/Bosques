2cf18d07fa37e14533f773d25ceb32f8f9bd409b
Angelini <angelini75@gmail.com>
2019-09-06 17:16:44 -0300

    elaboracion de matrices con latex

--- code/analisis_estadistico.R
+++ code/analisis_estadistico.R
@@ -223,6 +223,11 @@ region$LIArea <- region$LIArea/1000
 region$Area <- region$Area/1000
 region$LSArea <- region$LSArea/1000
 
+region$LIProporcion <- region$LIProporcion * 100
+region$Proporcion <- region$Proporcion * 100
+region$LSProporcion <- region$LSProporcion * 100
+
+kable(region, format = "latex", digits = 1)
 png("results/region.area.png", width = 2000, height = 1100, res = 200)
 ggplot(region, aes(x = Region, y = Area)) +
   geom_bar(stat = "identity", fill = "blue", color = "black", alpha = 0.5) + #facet_grid(~Region, scales = "free_x") + 
@@ -235,6 +240,7 @@ dev.off()
 
 # Resultado por provincia ########################################################
 e <- y %>% group_by(provincia) %>% 
+  filter(provincia %in% c("Salta", "Santiago del Estero", "Chaco", "Formosa")) %>% 
   # pi_i es la probabilidad de inclusión del segmento i en el estrato h
   # \hat{t)_h = sum{i=1}^{n_h} y_{hi}/pi_{hi} with 0
   # pi_{hi} = n_h/N_h 
@@ -369,6 +375,7 @@ y %>% group_by(code.umsef, code.unlu) %>% select(code.unlu, code.umsef, happ)  %
   geom_text(aes(label=paste0(round(sum,2),"%"))) + labs(x = "Clases de referencia",
                                             y = "Clases del mapa")
 
+
 y %>% group_by(region) %>% 
   mutate(area = sum(hap, na.rm = T)) %>% 
   group_by(code.umsef, code.unlu, region) %>%                                                             
@@ -402,7 +409,7 @@ y %>% group_by(provincia) %>%
                        guide=FALSE) + 
   geom_text(aes(label=paste0(round(perc,2),"%"))) + labs(x = "Clases de referencia",
                                                          y = "Clases del mapa") + 
-  facet_wrap(~provincia,)
+  facet_wrap(~provincia)
 
 # export to excell
 write.csv(row.names = F,
@@ -442,7 +449,7 @@ y$code.umsef <- as.factor(y$code.umsef)
 
 
 
-e <- y %>% group_by(region, code.unlu, ) %>% 
+e <- y %>% group_by(region, code.unlu ) %>% 
   # pi_i es la probabilidad de inclusión del segmento i en el estrato h
   # \hat{t)_h = sum{i=1}^{n_h} y_{hi}/pi_{hi} with 0
   # pi_{hi} = n_h/N_h 
@@ -458,7 +465,7 @@ e <- y %>% group_by(region, code.unlu, ) %>%
             var_hatt = (N_h^2) * var(ha)/n,# varianza de hatt
             error = qt(0.975, df = n - 1) * sqrt(var_hatt),
             hatt.ul.CI = hatt_h + error, # area upper limit confidence interval
-            hatt.ll.CI = hatt_h - error, # area lower limit confidence interval
+            hatt.ll.CI = hatt_h - error # area lower limit confidence interval
             ) %>% 
   select(code.unlu, n, -N_h, superficie = hatt_h, -var_hatt, error, LS = hatt.ul.CI, LI = hatt.ll.CI) %>% 
   mutate(code.unlu, superficie = round(superficie/1000,1),
@@ -496,6 +503,25 @@ dev.off()
 y$seg <- as.factor(y$seg)
 
 ########## análisis de concordancia entre interpretes #######
+clases <- tibble(clases = c( "TF_estable","OTF_estable", "OT_estable", 
+                             "TF_perdida_1998-2006",
+                             "OTF_perdida_1998-2006", "TF_perdida_2006-2013",  
+                             "OTF_perdida_2006-2013", "TF_perdida_2013-2017",  
+                             "OTF_perdida_2013-2017", "No_es_posible_determinar"),
+                 code = 1:10)
+
+
+y <- y %>% filter(unlu != "No_es_posible_determinar")
+y <- y %>% left_join(y = class, by = c("unlu" = "clas.name"))  
+y <- y %>% rename(code.unlu = clase)
+y <- y %>% left_join(y = class, by = c("umsef"="clas.name"))  
+y <- y %>% rename(code.umsef = clase)
+
+
+y$code.unlu <- as.factor(y$code.unlu)
+y$code.umsef <- as.factor(y$code.umsef)
+
+
 #seg.global <- 
   y %>% group_by(code.unlu) %>% select(code.unlu, seg, region, provincia) %>% 
   mutate(Concordancia = seg) %>% 
@@ -597,15 +623,147 @@ seg.formosa <- y %>% group_by(code.unlu) %>% select(code.unlu, seg, region, prov
 ggsave(filename = "results/seg.formosa.png", plot = seg.formosa, 
        width = 15, height = 10, units = "cm")  
 
-library(ggalluvial)
-y %>% #group_by(region) %>% filter(region == "smi") %>% 
-  group_by(code.umsef, code.unlu) %>% #filter(code.unlu %in% c(4,5,6,7,8,9) & code.umsef %in% c(4,5,6,7,8,9)) %>%                                                             
-  summarise(area = sum(hap, na.rm = T)/1000, Mapa = unique(code.umsef), Referencia = unique(code.unlu)) %>% 
-  group_by(Referencia, Mapa) %>% select(Referencia, Mapa, area) %>% 
-  ggplot(aes(axis1 = Mapa, axis2 = Referencia, y = area)) +
-  scale_x_discrete(limits = c("Referencia", "Mapa"), expand = c(.1, .05)) +
-  xlab("") +
-  geom_alluvium(aes(fill = Referencia)) +
-  geom_stratum() + geom_text(stat = "stratum", label.strata = TRUE) +
-  theme_minimal()
+### grafico por regiones
+y <- y %>% mutate(region=replace(region, region == "esp", "Espinal")) %>% 
+  mutate(region=replace(region, region == "pch", "Parque Chaqueño")) %>%
+  mutate(region=replace(region, region == "stb", "Selva Tucumano-Boliviana")) %>%
+  mutate(region=replace(region, region == "smi", "Selva Misionera"))
+
+seg.regiones <- y %>% group_by(code.unlu) %>% select(code.unlu, seg, region, provincia) %>% 
+  mutate(Concordancia = seg) %>%  
+  ggplot(aes(x= code.unlu,fill= Concordancia))+ geom_bar(position="fill", color = "black") +
+  geom_text(aes(label=..count..), size = 4, stat='count',
+            position = position_fill(vjust=0.5)) + xlab("Categorías") + 
+  ylab("Proporción") + scale_size_discrete("n") + scale_fill_brewer() +
+  theme(legend.position="bottom", legend.direction="horizontal", text = element_text(size=16)) + 
+  facet_wrap(~region, ncol = 1)
+
+ggsave(filename = "results/seg.regiones.png", plot = seg.regiones, 
+       width = 210, height = 300, units = "mm", dpi = 300)  
+
+### grafico por provincia
+seg.provincias <- y %>% group_by(code.unlu) %>% 
+  select(code.unlu, seg, region, provincia) %>% 
+  filter(provincia %in% c("Salta", "Santiago del Estero", "Chaco", "Formosa")) %>% 
+  mutate(Concordancia = seg) %>%  
+  ggplot(aes(x= code.unlu,fill= Concordancia))+ geom_bar(position="fill", color = "black") +
+  geom_text(aes(label=..count..), size = 4, stat='count',
+            position = position_fill(vjust=0.5)) + xlab("Categorías") + 
+  ylab("Proporción") + scale_size_discrete("n") + scale_fill_brewer() +
+  theme(legend.position="bottom", legend.direction="horizontal", text = element_text(size=16)) + 
+  facet_wrap(~provincia, ncol = 1)
+
+ggsave(filename = "results/seg.provincias.png", plot = seg.provincias, 
+       width = 210, height = 300, units = "mm", dpi = 300)  
+
+### grafico global
+seg.global <- y %>% group_by(code.unlu) %>% 
+  filter(provincia %in% c("Salta", "Santiago del Estero", "Chaco", "Formosa")) %>% 
+  mutate(Concordancia = seg) %>%  
+  ggplot(aes(x= code.unlu,fill= Concordancia))+ geom_bar(position="fill", color = "black") +
+  geom_text(aes(label=..count..), size = 4, stat='count',
+            position = position_fill(vjust=0.5)) + xlab("Categorías") + 
+  ylab("Proporción") + scale_size_discrete("n") + scale_fill_brewer() +
+  theme(legend.position="bottom", legend.direction="horizontal", text = element_text(size=16)) 
+ggsave(filename = "results/seg.provincias.png", plot = seg.provincias, 
+       width = 210, height = 100, units = "mm", dpi = 300)  
+
+############ producir matrices de confusión con sumas y estadísticos ###############
+
+clases <- tibble(clases = c( "TF_estable","OTF_estable", "OT_estable", 
+                             "TF_perdida_1998-2006",
+                             "OTF_perdida_1998-2006", "TF_perdida_2006-2013",  
+                             "OTF_perdida_2006-2013", "TF_perdida_2013-2017",  
+                             "OTF_perdida_2013-2017", "No_es_posible_determinar"),
+                 code = 1:10)
+
+
+y <- y %>% filter(unlu != "No_es_posible_determinar")
+y <- y %>% left_join(y = class, by = c("unlu" = "clas.name"))  
+y <- y %>% rename(code.unlu = clase)
+y <- y %>% left_join(y = class, by = c("umsef"="clas.name"))  
+y <- y %>% rename(code.umsef = clase)
+
+
+y$code.unlu <- as.factor(y$code.unlu)
+y$code.umsef <- as.factor(y$code.umsef)
+
+r <- unique(y$region)
+p <- as.character(unique(y$provincia)[c(7,8,13,15)])
+library(knitr)
+
+for (i in r) {
+a <- y %>% group_by(code.umsef, code.unlu ) %>% 
+  filter(region == i) %>%  
+  select(code.unlu,code.umsef, ha, pi_hi) %>% 
+  summarise(a = sum(ha/pi_hi, na.rm = TRUE)/1000) %>% 
+  select(code.unlu, code.umsef, a) %>% 
+  spread(key = code.unlu, value = a)
+a$Total <- NA
+a$Precision <- NA
+for (k in 1:9) {
+  a$Total[k] <- sum(a[k,2:10], na.rm = TRUE)
+  a[k,12] <- (a[k,1+k]/a$Total[k])*100
+  a[10,k+1] <- sum(a[,k+1], na.rm = TRUE)
+  a[11,k+1] <- (a[k,k+1] / a[10,k+1]) * 100
+}
+a$Total[10] <- sum(a$Total[1:9])
+a$code.umsef <- as.character(a$code.umsef)
+a[10,1] <- "Total"
+a[11,1] <- "Precision"
+names(a)[1] <- i
+print(kable(a,format = "markdown", digits = 2))
+}
+
+# Provincias
+for (i in p) {
+  a <- y %>% group_by(code.umsef, code.unlu ) %>% 
+    filter(provincia == i) %>%  
+    select(code.unlu,code.umsef, ha, pi_hi) %>% 
+    summarise(a = sum(ha/pi_hi, na.rm = TRUE)/1000) %>% 
+    select(code.unlu, code.umsef, a) %>% 
+    spread(key = code.unlu, value = a)
+  a$Total <- NA
+  a$Precision <- NA
+  for (k in 1:9) {
+    a$Total[k] <- sum(a[k,2:10], na.rm = TRUE)
+    a[k,12] <- (a[k,1+k]/a$Total[k])*100
+    a[10,k+1] <- sum(a[,k+1], na.rm = TRUE)
+    a[11,k+1] <- (a[k,k+1] / a[10,k+1]) * 100
+  }
+  a$Total[10] <- sum(a$Total[1:9])
+  a$code.umsef <- as.character(a$code.umsef)
+  a[10,1] <- "Total"
+  a[11,1] <- "Sencibilidad"
+  names(a)[1] <- i
+  print(kable(a,format = "latex", digits = 2))
+}
+
+# global
+a <- y %>% group_by(code.umsef, code.unlu ) %>% 
+  select(code.unlu,code.umsef, ha, pi_hi) %>% 
+  summarise(a = sum(ha/pi_hi, na.rm = TRUE)/1000) %>% 
+  select(code.unlu, code.umsef, a) %>% 
+  spread(key = code.unlu, value = a)
+a$Total <- NA
+a$Precision <- NA
+for (k in 1:9) {
+  a$Total[k] <- sum(a[k,2:10], na.rm = TRUE)
+  a[k,12] <- (a[k,1+k]/a$Total[k])*100
+  a[10,k+1] <- sum(a[,k+1], na.rm = TRUE)
+  a[11,k+1] <- (a[k,k+1] / a[10,k+1]) * 100
+}
+a$Total[10] <- sum(a$Total[1:9])
+a$code.umsef <- as.character(a$code.umsef)
+a[10,1] <- "Total"
+a[11,1] <- "Sensibilidad"
+names(a)[1] <- "global"
+
+x <- NULL
+for (i in 1:9) {
+ x <- append(x, as.numeric(a[i,i+1]))
+}
+a[11,12] <- sum(x)/a[10,11]*100
+print(kable(a,format = "latex", digits = 2))
+
   
